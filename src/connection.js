48d3389647ba9488c3addb2129c4328ee080a6e7
Ludmila <ludmilanrego@gmail.com>
2023-04-06 17:15:00 -0300

    dados de conexão no env

--- src/connection.js
+++ src/connection.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg")
 
 const pool = new Pool({
-    host: 'dpg-cgnhpcgdh87kgoaindf0-a.oregon-postgres.render.com',
-    port: 5432,
-    user: 'clarke',
-    password: 'ogrF8t35DWtatDi8eJS9lUrTqDCKWpYa',
-    database: 'clarke_afm9',
+    host: process.env.DBHOST,
+    port: process.env.DBPORT,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: process.env.DBDATABASE,
     ssl: {
         rejectUnauthorized: false
     }
