717aef4b513a07d9e7592b1cb183455f1aaa0552
Ludmila <ludmilanrego@gmail.com>
2023-04-06 17:09:02 -0300

    alteração password connection

--- src/connection.js
+++ src/connection.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     host: 'dpg-cgnhpcgdh87kgoaindf0-a.oregon-postgres.render.com',
     port: 5432,
     user: 'clarke',
-    password: '14YkQ49Ji21uKdBdJxszGrulfuADrCl1',
+    password: 'ogrF8t35DWtatDi8eJS9lUrTqDCKWpYa',
     database: 'clarke_afm9',
     ssl: {
         rejectUnauthorized: false
